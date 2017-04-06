76b3cfd2ea7b5c1dde829f1297605b553dafa64b
HerryPC <qianhw@mail.sim.ac.cn>
2017-04-06 20:00:11 +0800

    添加文字

---
+++ src/app/Surface/FontConst.c
@@ -0,0 +1,5 @@
+#include <platform.h>
+
+const char_t FONT_ZZWCSSB[]="自组网传输设备";
+
+const char_t FONT_GJ[]="关机";
---
+++ src/app/include/FontConst.h
@@ -0,0 +1,6 @@
+#ifndef FONT_CONST_H_
+#define FONT_CONST_H_
+
+extern const char_t FONT_ZZWCSSB[];
+extern const char_t FONT_GJ[];
+#endif
